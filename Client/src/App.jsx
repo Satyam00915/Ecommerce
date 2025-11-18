02729aac7cfa4fa10d67dcda33e380add586b76e
Satyam00915 <satyamupadhyay0495@gmail.com>
2025-11-18 18:14:54 +0530

    pages_init

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,7 +1,47 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import AuthLayout from "./components/auth/layout";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import AdminLayout from "./components/admin-view/layout";
+import Dashboard from "./pages/admin-view/Dashboard";
+import Products from "./pages/admin-view/Products";
+import Orders from "./pages/admin-view/Orders";
+import ShoppingLayout from "./components/shopping-view/layout";
+import NotFound from "./pages/not-found";
+import ShoppingHome from "./pages/shopping-view/home";
+import ShoppingCheckout from "./pages/shopping-view/checkout";
+import ShoppingListing from "./pages/shopping-view/listing";
+import ShoppingAccount from "./pages/shopping-view/account";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="flex flex-col overflow-hidden bg-white">
+      <h1>Header Component</h1>
+
+      <Routes>
+        <Route path="/auth" element={<AuthLayout />}>
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="products" element={<Products />} />
+          <Route path="orders" element={<Orders />} />
+        </Route>
+
+        <Route path="/shop" element={<ShoppingLayout />}>
+          <Route path="home" element={<ShoppingHome />} />
+          <Route path="checkout" element={<ShoppingCheckout />} />
+          <Route path="listing" element={<ShoppingListing />} />
+          <Route path="account" element={<ShoppingAccount />} />
+        </Route>
+
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
---
+++ Client/src/pages/not-found/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NotFound = () => {
+  return <div>Page Does not Exist</div>;
+};
+
+export default NotFound;
